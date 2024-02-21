94c8b558b01b19973733c224d1cc1f0137322548
AbbasMehrabi97 <abbasmehrabi95@gmail.com>
2024-02-22 01:15:14 +0300

    Character Manipulation

--- CharacterManipulation/main.cpp
+++ CharacterManipulation/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> //already included <cctype> library 
 using namespace std; //using standard library to avoid using std befor everything 
 int main(){
      /*
@@ -13,7 +13,7 @@ int main(){
      }else{
           cout << input_char <<  " is not alphanumeric." << endl;
      }*/
-
+     /*
      cout << "std::isalpha : " << endl;
 
      cout << "C is alphabetic : " << isalpha('C') << endl;
@@ -24,8 +24,82 @@ int main(){
           cout << 'e' <<  " is alphabetic." << endl;
      }else{
           cout << 'e' <<  " is not alphabetic." << endl;
-     }
+     }*/
+     /*
+     //Check if a character is blank
+     cout << "std::isblank : " << endl;
+     char message[] {"Hello there. How are you doing? The sun is shining."};
+     cout << "Message : " << message << endl;
 
+     int blank_count{};
+     for (size_t i = 0; i < size(message); i++)
+     {
+          if(isblank(message[i])){
+               cout << "Found a blank character at index : [" << i << "]" << endl;
+               ++blank_count;
+          }
+     }
+     cout << "In total we found : " << blank_count << " blank characters." << endl;*/
+     /*
+     //Check if character is lowercase or uppercase
+     cout << "std::islower and std::isupper : " << endl;
+     cout << endl;
+     char thought[] {"The C# Programing Language is one of the most used on the Planet"};
+     int lowercase_count{};
+     int uppercase_count{};
+
+     cout << "Original string : " << thought << endl;
+
+     for (auto character : thought)
+     {
+          if(islower(character)){
+               cout << " " << character;
+               ++lowercase_count;
+          }
+          if(isupper(character)){
+               ++uppercase_count;
+          }
+     }
+     cout << endl;
+     cout << "Found " << lowercase_count << " lowercase character and " << uppercase_count << 
+     " uppercase characters." << endl;*/
+     //Check if character is Digit
+     /*
+     cout << "std::isdigit : " << endl;
+     char statement[] {"Mr Abbas owns 323 cows. That is a lot of cows! The kid exclamed."};
+     cout << "Statement : " << statement << endl;
+
+     int digit_count{};
+
+     for (auto character : statement)
+     {
+          if(isdigit(character)){
+               cout << "Found digit : " << character << endl;
+               ++digit_count;
+          }
+     }
+     cout << "Found " << digit_count << " digits in the statement string." << endl;*/
+
+     //Turning a character to lowercase or uppercase
+     cout << "std::islower and std::isupper : " << endl;
+     cout << endl;
+     char orginal_str[] {"Home the feeling of blonging"};
+     char dest_str[size(orginal_str)];
+
+     //Turn to upper
+     for (size_t i = 0; i < size(orginal_str); i++)
+     {
+          dest_str[i] = toupper(orginal_str[i]);
+     }
+     cout << "The original string : " << orginal_str << endl;
+     cout << "Uppercase string : " << dest_str << endl;
+
+     //Turn to lowercase
+     for (size_t i = 0; i < size(orginal_str); i++)
+     {
+          dest_str[i] = tolower(orginal_str[i]);
+     }
+     cout << "Lowercase string : " << dest_str << endl;
      
 
      return 0;
