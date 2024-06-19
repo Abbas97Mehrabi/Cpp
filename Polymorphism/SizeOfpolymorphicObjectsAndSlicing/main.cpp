e6a5a8424ee38d02228ccb20075a7fc17daffc22
Abbas97Mehrabi <abbasmehrabi95@gmail.com>
2024-06-19 19:18:51 +0300

    size of polymophisic objects

---
+++ Polymorphism/SizeOfpolymorphicObjectsAndSlicing/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "shape.h"
+#include "oval.h"
+#include "circle.h"
+using namespace std;
+
+
+int main(){
+    //Comparing objects sizes
+    cout << "sizeof(Shape) : " << sizeof(Shape) << endl; //dynamic : 40
+    cout << "sizeof(Oval) : " << sizeof(Oval) << endl; //dynamic : 56
+    cout << "sizeof(Circle) : " << sizeof(Circle) << endl; //dynamic : 56
+
+    //Slicing
+    Circle circle1(3.3,"circle1");
+    Shape shape = circle1;
+    shape.draw();
+
+    return 0;
+}
