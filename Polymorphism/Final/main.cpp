8e79ae0952c2d314821b4d1651b1ba811ffc8130
Abbas97Mehrabi <abbasmehrabi95@gmail.com>
2024-06-23 19:22:05 +0300

    final function

---
+++ Polymorphism/Final/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "animal.h"
+#include "feline.h"
+#include "dog.h"
+#include "cat.h"
+#include "bird.h"
+#include "pigeon.h"
+#include "crow.h"
+using namespace std;
+
+//Intersting dact #1
+/*
+class Plane final
+{
+    Plane() = default;
+    
+};
+class FingerJet : public Plane
+{
+    FingerJet();
+    ~FingerJet();
+};*/
+
+int main(){
+    
+    
+    return 0;
+}    
+
+
+
